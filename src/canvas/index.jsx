66af36d9f14c858054f1ec143138b412e9903421
Matteo <preda.matteo.98@gmail.com>
2023-12-15 11:35:31 +0100

    Adjusted the behavior of the components: Backdrop,camerarig,shirt

--- src/canvas/index.jsx
+++ src/canvas/index.jsx
@@ -1,10 +1,31 @@
 import { Canvas } from '@react-three/fiber'
 import {Environment,Center} from '@react-three/drei'
+import Shirt from './Shirt';
+import  Backdrop  from './Backdrop';
+import CameraRig from './CameraRig'
 
 
 const CanvasModel = () => {
   return (
-    <div>Canvas</div>
+    /*all'interno di canvas non è possibile utilizzare div o altr elementi non facenti parti dell'albero della librerie THREE */
+    <Canvas 
+    shadows 
+    className="w-full max-w-full h-full transition-all ease-in" 
+    camera={{position:[0,0,0],fov: 25}} 
+    gl={{preserveDrawingBuffer: true}}>
+      <ambientLight intensity={0.5}/>
+      <Environment preset='city'/>
+
+      <CameraRig>
+
+        <Backdrop/>
+
+        <Center>
+          <Shirt/>
+        </Center>
+
+      </CameraRig>
+    </Canvas>
   )
 }
 
