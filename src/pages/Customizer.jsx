9c15749b375dab98c2ed2f45021fb5b2aeb73644
Matteo <preda.matteo.98@gmail.com>
2023-12-17 11:15:04 +0100

    added color picker and mechanism to enable editor tab

--- src/pages/Customizer.jsx
+++ src/pages/Customizer.jsx
@@ -12,6 +12,29 @@ import { CustomButton, AIPicker, ColorPicker, FilePicker, Tab } from '../compone
 
 const Customizer = () => {
   const snap = useSnapshot(state);
+
+  const [file, setFile] = useState('')
+  const [prompt, setPrompt] = useState('')//per l'AI
+  const [generatingImg, setGeneratingImg] = useState(false)
+  const [activeEditorTab, setActiveEditorTab] = useState('')
+  const [activeFilterTab, setActiveFilterTab] = useState({
+    logoShirt: true,
+    stylishShirt: false,
+  })
+
+  //show tab content depending on the activeTab 
+  const generateTabContent = () => {
+    switch(activeEditorTab){
+      case 'colorpicker':
+          return <ColorPicker/>
+      case 'filepicker':
+          return <FilePicker/>
+      case 'aipicker':
+          return <AIPicker/>
+      default:
+          null;
+    }
+  }
   return (
     <AnimatePresence>
       {!snap.intro && (
@@ -21,8 +44,9 @@ const Customizer = () => {
             <div className="flex items-center min-h-screen">
               <div className="editortabs-container tabs">
                 {EditorTabs.map((tab) => (
-                  <Tab key={tab.name} tab={tab} handleClick={() => { }} />
+                  <Tab key={tab.name} tab={tab} handleClick={() => {setActiveEditorTab(tab.name)}} />
                 ))}
+                {generateTabContent()}
               </div>
             </div>
           </motion.div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -23,7 +23,7 @@ const Home = () => {
                             </h1>
                         </motion.div>
                         <motion.div {...headContentAnimation} className="flex flex-col gap-5">
-                            <p className="max-w-md font-normal text-gray-600 text-base">Create your unique and exclusice shirt with our brand-new 3D customization tool <strong>Unleash your imagination</strong>{" "} and define your own style</p>
+                            <p className="max-w-md font-normal text-white text-base">Create your unique and exclusice shirt with our brand-new 3D customization tool <strong>Unleash your imagination</strong>{" "} and define your own style</p>
                             <CustomButton type="filled" title="Customize It" handleClick={()=>state.intro=false} customStyles="w-fit px-4 py-2.5 font-bold text-sm"/>
                         </motion.div>
 
